219085c77d3d8a92f0451ac205cd85765165c6f4
Bruce Qin <bruce.qin@outlook.com>
2022-08-21 19:10:54 -0700

    website skeleton

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Header.css';
+
+function Header() {
+  return (
+    <div className={styles.title}>
+        <h1> Bruce Qin </h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from './Home.css';
+import Header from '../components/Header/Header';
+
+function Home() {
+  return (
+    <div>
+    <Header></Header>
+    <div className="body">
+        
+        <h1>Test</h1>
+        Test
+    </div>
+    </div>
+  );
+}
+
+export default Home;
